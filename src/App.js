1c5607d1e61a399b3c8a6d1c2974ba53a61a490f
Gabriel Espinosa <gabiespinosa138@gmail.com>
2025-06-01 11:17:04 -0700

    Chore: basic constructions of card deck and storing in state, JSX render when mapping over state.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
+import { useEffect, useState } from "react";
+import { SUITS, VALUES } from "./assets/card-data";
+
+class Card {
+  constructor(suit, value) {
+    this.suit = suit;
+    this.value = value;
+  }
+  static random() {
+    const suit = SUITS[Math.floor(Math.random() * SUITS.length)];
+    const value = VALUES[Math.floor(Math.random() * VALUES.length)];
+    return new Card(suit, value);
+  }
+}
+
 function App() {
+  let [cards, setCards] = useState([]);
+
+  function buildDeck() {
+    let deck = []
+    for (let suit of SUITS) {
+      for (let value of VALUES) {
+        deck.push(new Card(suit, value));
+      }
+    }
+    setCards(deck)
+  }
+
+  useEffect(() => {
+    buildDeck();
+  }, []);
+
   return (
     <div>
       <h1>Hello World!</h1>
+      {cards.map((card) => (<div>{card.value}</div>))}
     </div>
   );
 }
---
+++ src/assets/card-data.js
@@ -0,0 +1,8 @@
+
+//card data arrays
+
+const SUITS = ['Clubs', 'Spades', 'Hearts', 'Diamonds'];
+const VALUES = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace'];
+
+
+export {SUITS, VALUES}
