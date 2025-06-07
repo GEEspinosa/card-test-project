6c41f1f3b5dd341a60151ad17d4d5d816ec6f951
Gabriel Espinosa <gabiespinosa138@gmail.com>
2025-06-07 14:34:56 -0700

    chore: added draw button with function poping from stack

--- src/App.js
+++ src/App.js
@@ -1,30 +1,33 @@
 import { useEffect, useState } from "react";
-import { SUITS, VALUES } from "./assets/card-data";
+import { SUITS, RANKS } from "./assets/card-data";
 
 class Card {
-  constructor(suit, value) {
+  constructor(suit, rank) {
     this.suit = suit;
-    this.value = value;
+    this.rank = rank;
   }
-  static random() {
-    const suit = SUITS[Math.floor(Math.random() * SUITS.length)];
-    const value = VALUES[Math.floor(Math.random() * VALUES.length)];
-    return new Card(suit, value);
-  }
+
+  //dev note: not sure I need this given the shuffleDeck() function
+  // static random() {
+  //   const suit = SUITS[Math.floor(Math.random() * SUITS.length)];
+  //   const rank = RANKS[Math.floor(Math.random() * RANKS.length)];
+  //   return new Card(suit, rank);
+  // }
 }
 
 function App() {
   let [cards, setCards] = useState([]);
-  //let [selected, setSelected] = useState()
+  let [selected, setSelected] = useState({suit: null, rank: null})
 
   function buildDeck() {
     let deck = [];
     for (let suit of SUITS) {
-      for (let value of VALUES) {
-        deck.push(new Card(suit, value));
+      for (let rank of RANKS) {
+        deck.push(new Card(suit, rank));
       }
     }
     setCards(deck);
+    setSelected(deck[0])
   }
 
   function shuffleDeck() {
@@ -34,8 +37,18 @@ function App() {
       const j = Math.floor(Math.random() * (i + 1));
       [deck[i], deck[j]] = [deck[j], deck[i]]
     }
-    console.log(deck)
     setCards(deck)
+  }
+
+  function drawCard() {
+    let deck = cards;
+    console.log(deck)
+    if (deck.length){
+      setSelected(deck.pop())
+    } else {
+      setSelected({suit: 'shuffle', rank: 'again'})
+    }
+    
   }
 
   useEffect(() => {
@@ -45,6 +58,9 @@ function App() {
   return (
     <div>
       <h1 style={{display: "flex", justifyContent: "center"}}>Hello World!</h1>
+      
+      
+      
       <div
         style={{
           display: "flex",
@@ -53,6 +69,9 @@ function App() {
           flexWrap: "wrap",
         }}
       >
+
+        {/* dev note: this displays entire card deck */}
+{/*         
         {cards.map((card) => (
           <div
             style={{
@@ -68,12 +87,35 @@ function App() {
             }}
           >
             <div>{card.suit}</div>
-            <div>{card.value}</div>
+            <div>{card.rank}</div>
           </div>
-        ))}
+        ))} */}
+
+
+        
+        <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              flexDirection: "column",
+              backgroundColor: "salmon",
+              padding: "15px",
+              margin: "10px",
+              width: "80px",
+              height: "120px",
+              borderRadius: "8px",
+            }}
+          >
+            <div>{selected.suit}</div>
+            <div>{selected.rank}</div>
+        </div>
+       
       </div>
       <button onClick = {shuffleDeck}>
         Shuffle
+      </button>
+      <button onClick = {drawCard}>
+        Draw
       </button>
     </div>
   );
--- src/assets/card-data.js
+++ src/assets/card-data.js
@@ -2,7 +2,7 @@
 //card data arrays
 
 const SUITS = ['♠', '♣', '♥', '♦'];
-const VALUES = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'];
+const RANKS = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'];
 
 
-export {SUITS, VALUES}
+export {SUITS, RANKS}
