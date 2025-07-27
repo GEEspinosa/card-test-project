8b2926f742fce891b87052840679ceb0055e5170
Gabriel Espinosa <gabiespinosa138@gmail.com>
2025-07-27 01:37:51 -0700

    Chore: added a toggle button on Event Log

--- src/App.js
+++ src/App.js
@@ -39,6 +39,7 @@ function App() {
   let [playerTwoVictories, setPlayerTwoVictories] = useState(0);
   let [message, setMessage] = useState("...waiting for card draw");
   let [log, setLog] = useState([]);
+  let [logOpen, setLogOpen] = useState(true)
 
   const logEndRef = useRef(null);
 
@@ -468,6 +469,10 @@ function App() {
     );
   }
 
+  function logButtonHandler () {
+    setLogOpen((prev) => !prev)
+  }
+
   const warStateMap = {
     [WAR_STATES.NONE]: {
       handler: drawCard,
@@ -593,14 +598,18 @@ function App() {
 
       <div className="log-section">
         <h3>Event Log</h3>
-        <div className="log-box">
+        <div className={`log-button ${logOpen ? "open" : ""}`} onClick = {logButtonHandler}>
+          <div className="log-button-line left"></div>
+          <div className="log-button-line right"></div>
+        </div>
+        {logOpen && (<div className="log-box">
           <ul>
             {log.map((entry, i) => (
               <li key={i}>{entry}</li>
             ))}
             <div ref={logEndRef} />
           </ul>
-        </div>
+        </div>)}
       </div>
     </div>
   );
