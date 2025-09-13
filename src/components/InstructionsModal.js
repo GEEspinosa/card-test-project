4554391c69b24a3d1d67317ba5061e309ac12942
Gabriel Espinosa <gabiespinosa138@gmail.com>
2025-09-13 12:03:18 -0700

    fix: added useEffect with vanilla JS instructions to fix/freeze screen when instructions modal is open.

--- src/components/InstructionsModal.js
+++ src/components/InstructionsModal.js
@@ -1,6 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 export default function InstructionsModal({ onClose }) {
+
+//below ensures open instructions modal freezes any scrolling in browser
+  useEffect(() => {
+    const scrollY = window.scrollY;
+    document.body.style.position = "fixed";
+    document.body.style.top = `-${scrollY}px`;
+    document.body.style.left = "0";
+    document.body.style.right = "0";
+    document.body.style.overflow = "hidden";
+
+    return () => {
+      const scrollY = Math.abs(parseInt(document.body.style.top || "0"));
+      document.body.style.position = "";
+      document.body.style.top = "";
+      document.body.style.left = "";
+      document.body.style.right = "";
+      document.body.style.overflow = "";
+      window.scrollTo(0, scrollY);
+    };
+  }, []);
   return (
     <div className="instruction-modal-overlay">
       <div className="instruction-modal-content">
